a7eda9e439e4811c4a3c8fe51c59b9bb519cae53
kurticognjen <kurticognjen@outlook.com>
2016-12-12 21:50:41 +0100

    Added note component

---
+++ ngBoardFrontend/src/app/note/note.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent { }
