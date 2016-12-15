9b0edf1aa6d6bbe84868cd4f3342d24358338cfd
kurticognjen <kurticognjen@outlook.com>
2016-12-15 20:36:27 +0100

    added scss to note component

--- ngBoardFrontend/src/app/note/note.component.ts
+++ ngBoardFrontend/src/app/note/note.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.css']
+  styleUrls: ['./note.component.scss']
 })
 export class NoteComponent {
 
