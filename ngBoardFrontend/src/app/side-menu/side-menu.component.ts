fae4f9b4165b7031279057b81818546b4efaee64
kurticognjen <kurticognjen@outlook.com>
2016-12-21 23:25:26 +0100

    Refactored dynamic adding of sideMenu classes

--- ngBoardFrontend/src/app/side-menu/side-menu.component.ts
+++ ngBoardFrontend/src/app/side-menu/side-menu.component.ts
@@ -9,18 +9,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class SideMenuComponent {
   @Input() menuItems: string[];
   @Input() otherItems: string[];
-  @Output() menuClicked = new EventEmitter<string>(); 
+  @Output() menuClicked = new EventEmitter<string>();
+
   sideMenuOpened: boolean = true;
-  sideMenuButtonText: string = '<';
-  sideMenuExpanded = 'sideMenu expanded';
-  sideMenuContracted = 'sideMenu contracted';
 
-  sideMenuClasses: string = this.sideMenuExpanded;
-
-  sideMenu() {
-    this.sideMenuClasses = this.sideMenuOpened ? this.sideMenuContracted : this.sideMenuExpanded;
-    this.sideMenuButtonText = this.sideMenuOpened ? '>' : '<';
-
+  toggleSideMenu() {
     this.sideMenuOpened = !this.sideMenuOpened;
   }
 
