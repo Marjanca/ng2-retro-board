f1b433864244692c17f7b3f3acbe653d7689b240
Milos Stankovic <milos.stankovic@zuehlke.com>
2016-12-28 21:07:23 +0100

    drag-n-drop first working version

--- ngBoardFrontend/src/app/app.module.ts
+++ ngBoardFrontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NoteComponent } from './note/note.component';
+import { DragNDropDirective } from './note/dragNDrop/drag-n-drop.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteComponent
+    NoteComponent,
+    DragNDropDirective
   ],
   imports: [
     BrowserModule,
