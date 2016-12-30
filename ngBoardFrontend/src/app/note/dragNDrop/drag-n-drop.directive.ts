e799141a821fce33f0dd53cb0ad254416205a74c
Milos Stankovic <milos.stankovic@zuehlke.com>
2016-12-30 14:25:50 +0100

    used host listeners instead of host

--- ngBoardFrontend/src/app/note/dragNDrop/drag-n-drop.directive.ts
+++ ngBoardFrontend/src/app/note/dragNDrop/drag-n-drop.directive.ts
@@ -1,44 +1,39 @@
-import { Directive, ElementRef, Input, HostListener, Renderer } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[appDragNDrop]',
-  host: {
-    '(dragstart)': 'onDragStart($event)',
-    '(dragover)': 'onDragOver($event)',
-    '(drop)': 'onDrop($event)',
-  }
+  selector: '[appDragNDrop]'
 })
 export class DragNDropDirective {
   private static noteIdGen: number = 0;
   constructor(el: ElementRef, renderer: Renderer) {
     el.nativeElement.draggable = true;
-    renderer.setElementAttribute(el.nativeElement, "note-id", (DragNDropDirective.noteIdGen++).toString());
+    renderer.setElementAttribute(el.nativeElement, 'note-id', (DragNDropDirective.noteIdGen++).toString());
   }
-  onDragOver(ev) {
+  @HostListener('dragover', ['$event']) onDragOver(ev) {
     console.log('allow drop');
     ev.preventDefault();
   };
 
-  onDragStart(ev) {
+  @HostListener('dragstart', ['$event']) onDragStart(ev) {
     console.log('drag');
 
-    ev.dataTransfer.setData("text/plain", ev.target.attributes["note-id"].nodeValue);
+    ev.dataTransfer.setData('text/plain', ev.target.attributes['note-id'].nodeValue);
   };
 
-  onDrop(ev) {
+  @HostListener('drop', ['$event']) onDrop(ev) {
     console.log('drop');
 
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    var elem = <HTMLElement>document.querySelector("[note-id='" + data + "']");
+    let data = ev.dataTransfer.getData('text');
+    let elem = <HTMLElement>document.querySelector('[note-id="' + data + '"]');
 
-    var tabla = document.getElementById("tabla");
+    let tabla = document.getElementById('tabla');
 
-    var kockicaRect = elem.getBoundingClientRect();
-    var tablaRect = tabla.getBoundingClientRect();
+    // var kockicaRect = elem.getBoundingClientRect();
+    let tablaRect = tabla.getBoundingClientRect();
 
-    elem.style.left = (ev.pageX - tablaRect.left).toString() + "px";
-    elem.style.top = (ev.pageY - tablaRect.top).toString() + "px";
+    elem.style.left = (ev.pageX - tablaRect.left).toString() + 'px';
+    elem.style.top = (ev.pageY - tablaRect.top).toString() + 'px';
     elem.style.zIndex = '10000';
   };
 }
