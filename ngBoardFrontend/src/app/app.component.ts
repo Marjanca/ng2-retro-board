0ef9bb08698ee3fa71a2dedce83dcc5ee86073ad
kurticognjen <kurticognjen@outlook.com>
2017-01-08 19:50:22 +0100

    Added board entity and mock data

--- ngBoardFrontend/src/app/app.component.ts
+++ ngBoardFrontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardMenuItem } from './side-menu/models/boardMenuItem';
+import { Board } from './board/models/board';
 import { Note } from './note/models/note';
 import { NoteCoords } from './note/models/note-coords';
 
@@ -10,24 +11,54 @@ import { NoteCoords } from './note/models/note-coords';
 })
 export class AppComponent implements OnInit {
 
-  private boards: BoardMenuItem[];
-  private activeBoardNotes: Note[];
+  private boardMenuItems: BoardMenuItem[];
+  private boards: Board[] = [];
 
   ngOnInit() {
-    this.boards = [
-      {id: 1, name: 'First'},
-      {id: 2, name: 'Second'},
-      {id: 3, name: 'Third'}
-    ];
-
-    this.activeBoardNotes = [
-      {text: 'first note', author: 'first author', coords: new NoteCoords(100, 100, 0)},
-      {text: 'second note', author: 'second author', coords: new NoteCoords(200, 200, 0)},
-      {text: 'third note', author: 'third author', coords: new NoteCoords(300, 300, 0)}
-    ];
+    this.setupMockData();
+    this.boardMenuItems = this.boards.map((board) => {
+      return { id: board.Id, name: board.Title };
+    });
   }
 
   menuItemClicked(item: BoardMenuItem) {
     console.log(item);
+  }
+
+  setupMockData() {
+    let firstBoardNotes = [
+       {text: 'first note', author: 'first author', coords: new NoteCoords(100, 100, 0)},
+       {text: 'second note', author: 'second author', coords: new NoteCoords(200, 200, 0)},
+       {text: 'third note', author: 'third author', coords: new NoteCoords(300, 300, 0)}
+    ];
+
+    let firstBoard = new Board(1, 'First board', 'First author');
+    firstBoard.Notes = firstBoardNotes;
+
+    this.boards.push(firstBoard);
+
+    let secondBoardNotes = [
+       {text: 'Batman', author: 'Batman', coords: new NoteCoords(100, 100, 0)},
+       {text: 'is', author: 'Batman', coords: new NoteCoords(78, 129, 0)},
+       {text: 'the', author: 'Batman', coords: new NoteCoords(250, 220, 0)},
+       {text: 'king!', author: 'Batman', coords: new NoteCoords(431, 322, 0)}
+    ];
+
+    let secondBoard = new Board(2, 'Batman board ', 'Batman');
+    secondBoard.Notes = secondBoardNotes;
+
+    this.boards.push(secondBoard);
+
+    let thirdBoardNotes = [
+       {text: 'Chungas Revenge', author: 'Frank Zappa', coords: new NoteCoords(431, 200, 0)},
+       {text: '200 Motels', author: 'Frank Zappa', coords: new NoteCoords(333, 248, 0)},
+       {text: 'One Size Fits All', author: 'Frank Zappa', coords: new NoteCoords(123, 223, 0)},
+       {text: 'Ship Arriving Too Late to Save a Drowning Witch ', author: 'Frank Zappa', coords: new NoteCoords(188, 345, 0)}
+    ];
+
+    let thirdBoard = new Board(3, 'Zappa board ', 'Frank');
+    thirdBoard.Notes = thirdBoardNotes;
+
+    this.boards.push(thirdBoard);
   }
 }
---
+++ ngBoardFrontend/src/app/board/models/board.ts
@@ -0,0 +1,26 @@
+import { Note } from '../../note/models/note';
+
+export class Board {
+    private id: number;
+    private title: string;
+    private notes: Note[];
+    private creator: string;
+
+    constructor(id: number, title: string, creator: string) {
+        this.id = id;
+        this.title = title;
+        this.creator = creator;
+    }
+
+    set Notes(notes: Note[]) {
+        this.notes = notes;
+    }
+
+    get Id() {
+        return this.id;
+    }
+
+    get Title() {
+        return this.title;
+    }
+}
