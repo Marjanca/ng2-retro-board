10f324c2e59eba64dc1510887c22148bce81a456
Djordje Madic <madj@zuehlke.com>
2017-01-10 13:43:37 +0100

    Added Note event emission test

--- ngBoardFrontend/src/app/board/add-note-modal/add-note-modal.component.spec.ts
+++ ngBoardFrontend/src/app/board/add-note-modal/add-note-modal.component.spec.ts
@@ -1,3 +1,6 @@
+import { NoteCoords } from './../../note/models/note-coords';
+import { Note } from './../../note/models/note';
+import { FormsModule } from '@angular/forms';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -11,9 +14,12 @@ describe('AddNoteModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddNoteModalComponent ]
+      imports: [
+        FormsModule
+      ],
+      declarations: [AddNoteModalComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,4 +31,18 @@ describe('AddNoteModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit onNoteCreate event when Post button is clicked ', () => {
+    let receivedNote;
+    component.onNoteCreated.subscribe(emittedNote => receivedNote = emittedNote);
+
+    let expectedNote = new Note('some text', 'some author', new NoteCoords(100, 200, 1));
+    component.note = expectedNote;
+    
+    let postButton = fixture.debugElement.query(By.css('.data-test-post-button'));
+    postButton.triggerEventHandler('click', null);
+
+    expect(receivedNote).toBe(expectedNote);
+  });
+
 });
