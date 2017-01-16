c4c207fc5dd41dec87d46caebf19ce69ec49f25d
Djordje Madic <madj@zuehlke.com>
2017-01-16 14:28:08 +0100

    simple modal component

--- ngBoardFrontend/src/app/shared/modal/modal.component.ts
+++ ngBoardFrontend/src/app/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, Renderer } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string;
+  @Output() submit: EventEmitter<any> = new EventEmitter();
+
+  private modal: any;
+
+  constructor(private element: ElementRef, private renderer: Renderer) {
+    this.modal = element.nativeElement.querySelector('.modal');
+  }
 
   ngOnInit() {
+    this.renderer.listenGlobal('document', 'click', (event) => {
+      if (event.target === this.modal) {
+        this.close();
+      }
+    });
+  }
+
+  private close() {
+    this.modal.style.display = 'none';
+  }
+
+  onClose() {
+    this.close();
+  }
+
+  onSubmit() {
+    this.submit.emit(null);
   }
 
 }
