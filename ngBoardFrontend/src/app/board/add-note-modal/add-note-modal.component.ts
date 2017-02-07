ce6ad1051756c6ac29a6cc689ec35c91e3cfae4b
Djordje Madic <madj@zuehlke.com>
2017-02-07 17:05:59 +0100

    Replaced template variable with binding

--- ngBoardFrontend/src/app/board/add-note-modal/add-note-modal.component.ts
+++ ngBoardFrontend/src/app/board/add-note-modal/add-note-modal.component.ts
@@ -4,6 +4,10 @@ import { NoteCoords } from './../../note/models/note-coords';
 import { Note } from './../../note/models/note';
 import { ModalComponent } from './../../shared/modal/modal.component';
 
+function createDefaultNote(): Note {
+  return new Note('', '<anonymous>', new NoteCoords(200, 250, 1000));
+}
+
 @Component({
   selector: 'app-add-note-modal',
   templateUrl: './add-note-modal.component.html',
@@ -12,9 +16,10 @@ import { ModalComponent } from './../../shared/modal/modal.component';
 export class AddNoteModalComponent {
 
   @ViewChild('modal') modal: ModalComponent;
-  @ViewChild('textarea') textArea: ElementRef;
 
   @Output() noteCreated: EventEmitter<Note> = new EventEmitter();
+
+  note: Note = createDefaultNote();
 
   public openModal() {
     this.modal.openModal();
@@ -22,11 +27,12 @@ export class AddNoteModalComponent {
 
   onClose() {
     console.log('Add note modal closed.');
-    this.textArea.nativeElement.value = '';
+    this.note = createDefaultNote();
   }
 
   onSubmit() {
     console.log('Add note modal submited.');
-    this.noteCreated.emit(new Note(this.textArea.nativeElement.value, '<anonymous>', new NoteCoords(250, 200, 10000)));
+    console.dir(this.note);
+    this.noteCreated.emit(this.note);
   }
 }
