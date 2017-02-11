b654c15203aa1be631b445d4ef95d216512738f2
Djordje Madic <madj@zuehlke.com>
2017-02-11 18:30:19 +0100

    reordered imports

--- ngBoardFrontend/src/app/app.component.spec.ts
+++ ngBoardFrontend/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { SideMenuModule } from './side-menu/side-menu.module';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { SideMenuModule } from './side-menu/side-menu.module';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { NoteComponent } from './note/note.component';
 import { BoardModule } from './board/board.module';
