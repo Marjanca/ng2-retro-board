f8147f77e59c162404f3a534434eb18de42ca3fa
kurticognjen <kurticognjen@outlook.com>
2017-02-16 18:04:33 +0100

    Moved auth.service to shared module

--- ngBoardFrontend/src/app/login/login.module.ts
+++ ngBoardFrontend/src/app/login/login.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from '../shared/services/auth/auth.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,13 +11,11 @@ import { AuthService } from '../shared/services/auth/auth.service';
   ],
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     LoginComponent
-  ],
-  providers: [
-    AuthService
   ]
 })
 export class LoginModule { }
--- ngBoardFrontend/src/app/shared/shared.module.ts
+++ ngBoardFrontend/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ModalComponent } from './modal/modal.component';
+import { AuthService } from './services/auth/auth.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [ModalComponent],
-  exports: [ModalComponent]
+  exports: [ModalComponent],
+  providers: [
+    AuthService
+  ]
 })
 export class SharedModule { }
