b23944532ac7b832bc4acd64c05a11f1bc2472a1
Djordje Madic <madj@zuehlke.com>
2017-02-17 17:31:27 +0100

    fixed lint

--- ngBoardFrontend/src/app/shared/constants.ts
+++ ngBoardFrontend/src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 
 
 // Default coordinates when creating note by pressing a button
-export const NOTE_DEFAULT_TOP: number = 100;
-export const NOTE_DEFAULT_LEFT: number = 200;
+export const NOTE_DEFAULT_TOP = 100;
+export const NOTE_DEFAULT_LEFT = 200;
